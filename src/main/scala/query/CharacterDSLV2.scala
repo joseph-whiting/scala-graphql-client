35ab1f6b1b93599fd8fa3b296da56fb0f25694eb
Joseph Whiting <whitinjo.07@hotmail.co.uk>
2018-04-07 18:17:57 +0100

    luke with age and name, decent dummy of the dsl type safety

--- src/main/scala/query/CharacterDSLV2.scala
+++ src/main/scala/query/CharacterDSLV2.scala
@@ -1,22 +1,41 @@
-class WithField[F <: Field] {
-  implicit def innerObject[T](outer: TypedWithField[T]): T = outer.inner
-  def ::[T](typed: T) = new TypedWithField[T](typed)
-  class TypedWithField[T] (val inner: T) extends Name
+object WithName {
+  implicit def innerObject[T](outer: TypedWithName[T]): T = outer.inner
+  def ::[T](typed: T) = new TypedWithName[T](typed)
+  class TypedWithName[T] (val inner: T) extends Name
+}
+
+object WithAge {
+  implicit def innerObject[T](outer: TypedWithAge[T]): T = outer.inner
+  def ::[T](typed: T) = new TypedWithAge[T](typed)
+  class TypedWithAge[T] (val inner: T) extends Age
 }
 
 class Character {}
 
-abstract trait Field
+
+trait Field
+
 trait Name extends Field {
   var name: String = "Luke"
 }
-
-object WithName extends WithField[Name] {}
+trait Age extends Field {
+  var age: Integer = 23
+}
 
 object Luke extends Character {}
 
 object Test {
-  val lukeWithName = (Luke :: WithName)
+  val lukeWithName = Luke :: WithName
   lukeWithName.name = "Han"
   val lukesName = lukeWithName.name
+
+  val lukeWithAge = Luke :: WithAge
+  lukeWithAge.age = 22
+  val lukesAge = lukeWithAge.age
+
+  val lukeWithAgeAndName = (Luke :: WithAge) :: WithName
+
+  lukeWithAgeAndName.age = 28
+  val age2 = lukeWithAgeAndName.age
+  val name2 = lukeWithAgeAndName.name
 }
