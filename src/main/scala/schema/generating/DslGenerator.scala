9af0feb6338714ed744609ef6ef2c587046e5dcd
Joseph Whiting <whitinjo.07@hotmail.co.uk>
2018-04-10 11:06:06 +0100

    added travis yml

--- src/main/scala/schema/generating/DslGenerator.scala
+++ src/main/scala/schema/generating/DslGenerator.scala
@@ -1,7 +1,10 @@
 package scalagraphqlclient.schema.generating
 import scalagraphqlclient.schema.parsing._
 
-abstract class Generator {
-  def generate(types: Seq[TypeDefinition]): String
+class Generator {
+  def generate(types: Seq[TypeDefinition]): String = """object WithName {
+  implicit def innerObject[T](outer: TypedWithName[T]): T = outer.inner
+  def ::[T](typed: T) = new TypedWithName[T](typed)
+  class TypedWithName[T] (val inner: T) extends Name
+}"""
 }
-
